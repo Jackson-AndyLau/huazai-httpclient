de349ca1ccb0599b34769834724e875afa12567a
panda <dl.herbert@foxmail.com>
2019-11-19 17:49:44 +0800

    优化DoPost测试单元

--- src/main/java/com/huazai/httpclient/post/DoPOST.java
+++ src/main/java/com/huazai/httpclient/post/DoPOST.java
@@ -30,7 +30,7 @@ public class DoPOST
 
 		// 创建HttpPost请求对象
 		HttpPost httpPost = new HttpPost("http://www.oschina.net/");
-		
+
 		// 设置请求头
 		httpPost.setHeader("User-Agent",
 				"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36");
